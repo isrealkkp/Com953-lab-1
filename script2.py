0d453c62867c21de16b1dc8bf602f16dcc77465f
isrealkkp <156540197+isrealkkp@users.noreply.github.com>
2024-01-16 11:45:52 -0500

    Modified both file in the repo clone
    Deleted a print() statement from script1.py
    Added two print() statements to script2.py

--- script2.py
+++ script2.py
@@ -1,2 +1,4 @@
 #comment
 print('if the implementation is hard to explain, it,s a bad idea.')
+print('if the implementation is easy to explain, it may be a good idea.')
+print("Namespaces are one honking great idea -- let's do more of those")
